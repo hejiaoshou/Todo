a2de3fc0e3aeb221e533fe6636aeb569c1e5cbc5
hjs <hdhwr@live.com>
2018-12-11 18:35:05 +0800

    addUser

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const uploadfile = require('./files/uploadFIle')
+const user = require('./user/index')
+router.use((req, res, next) => {
+  console.log(req.url)
+  next()
+})
+
+router.get('/',(req, res) => {
+  res.send("hello")
+})
 
 router.post('/uploadFIles',uploadfile)
 
-router.post('/user/*',(req, res) => {
-  res.send('user')
-})
+router.get('/user/*',user)
 
 module.exports = router
